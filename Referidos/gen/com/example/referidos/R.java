b2e1b2182eb7b965a84631dc6d3f12fa632a271a
andreska514 <andruscapa4@hotmail.com>
2014-12-04 13:51:33 +0100

    proyect

--- Referidos/gen/com/example/referidos/R.java
+++ Referidos/gen/com/example/referidos/R.java
@@ -1094,12 +1094,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int check_estudio=0x7f050042;
-        public static final int check_trabajar=0x7f050043;
+        public static final int btnAdd=0x7f05003e;
+        public static final int check_estudio=0x7f050043;
+        public static final int check_trabajar=0x7f050044;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1118,7 +1119,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
-        public static final int listView1=0x7f05003e;
+        public static final int listView1=0x7f05003f;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1127,8 +1128,8 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int refNombre=0x7f05003f;
-        public static final int refTelefono=0x7f050041;
+        public static final int refNombre=0x7f050040;
+        public static final int refTelefono=0x7f050042;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1143,7 +1144,7 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
-        public static final int spinner_parentesco=0x7f050040;
+        public static final int spinner_parentesco=0x7f050041;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
--- Referidos/src/com/example/referidos/Main.java
+++ Referidos/src/com/example/referidos/Main.java
@@ -7,54 +7,81 @@ import android.view.MenuItem;
 
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Main extends ActionBarActivity {
-
+	EditText editTuNombre;
+	EditText editFecha;
+	Button btnAdd;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Display a indeterminate progress bar on title bar
 	    requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-	    
 		setContentView(R.layout.activity_main);
-		//final ProgressDialog progressDialog = new ProgressDialog(this);
+		
+		editTuNombre = (EditText)findViewById(R.id.editTuNombre);
+		editFecha = (EditText)findViewById(R.id.editFecha);
+		btnAdd = (Button)findViewById(R.id.btnAdd);
+		
+		btnAdd.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//FragmentManager fragmentManager = getSupportFragmentManager();
+		        DialogoPersonalizado dialogo = new DialogoPersonalizado();
+		        dialogo.show(getFragmentManager(), "tagAlerta");
+		        //dialogo.show(fragmentManager, "tagAlerta");
+			}
+		});
 		final ListView listView = (ListView)findViewById(R.id.listView1);
 		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 		    @Override
 		    public void onItemClick(AdapterView adapter, View view, int position, long arg) {
 		 
-		        // Sets the visibility of the indeterminate progress bar in the
-		        // title
+		        // Sets the visibility of the progress bar in the title
 		        setProgressBarIndeterminateVisibility(true);
-		 
 		        // Show progress dialog
 		        ProgressDialog progressDialog = ProgressDialog.show(Main.this, "ProgressDialog", "Loading!");
-		 
-		        // Tells JavaScript to open windows automatically.
-		        //webView.getSettings().setJavaScriptEnabled(true);
-		 
-		        // Sets our custom WebViewClient.
-		        //webView.setWebViewClient(new myWebClient());
-		 
 		        // Loads the given URL
 		        Item item = (Item) listView.getAdapter().getItem(position);
-		           // webView.loadUrl(item.getUrl());
 		    }
 		});
+	}
+	class DialogoPersonalizado extends DialogFragment {
+	    @Override
+	    public Dialog onCreateDialog(Bundle savedInstanceState) {
+	 
+		    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		    LayoutInflater inflater = getActivity().getLayoutInflater();
+		 
+		    builder.setView(inflater.inflate(R.layout.list, null))
+		       .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                  dialog.cancel();
+		           }
+		    });
+		    return builder.create();
+	    }
 	}
 
 	class ItemAdapter extends BaseAdapter {
